b98c88e5c29b1b247a719e1a54d2dc561f0664bd
amandaratio <Amanda@Amandas-MacBook-Pro.local>
2017-08-21 13:29:48 -0700

    database model .. tested

--- reserve/models.py
+++ reserve/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class SanFranTrain(models.Model):
+    TO_LOC = (
+        ('A', 'LA'),
+        ('B', 'SJ'),
+        ('C', 'FR'),
+        ('D', 'AN'),
+              #add more to locs ...
+    )
+    to = models.CharField(max_length = 1, choices=TO_LOC,)
+    date = models.DateField()
+    time = models.CharField(max_length = 6)
+    seats = models.CharField(max_length = 3)
+
