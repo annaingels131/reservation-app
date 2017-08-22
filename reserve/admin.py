6ec0bd53061d2f815f069d9edb2159077d006c41
amandaratio <Amanda@Amandas-MacBook-Pro.local>
2017-08-21 19:47:01 -0700

    migrations etc

--- reserve/admin.py
+++ reserve/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import SFTrain
+from .models import SanFranciscoTrain, MilroyTrain, GilroyTrain, SanJoseTrain, MercedTrain, FresnoTrain, KingstonTrain, BakersfieldTrain, PalmdaleTrain, BurbankTrain, LosAngelesTrain, OrangeCountyTrain, AnaheimTrain
 
-# Register your models here.
-admin.site.register(SFTrain)
-admin.site.register(
+# register models
+admin.site.register(SanFranciscoTrain)
+admin.site.register(MilroyTrain)
+admin.site.register(GilroyTrain)
+admin.site.register(SanJoseTrain)
+admin.site.register(MercedTrain)
+admin.site.register(FresnoTrain)
+admin.site.register(KingstonTrain)
+admin.site.register(BakersfieldTrain)
+admin.site.register(PalmdaleTrain)
+admin.site.register(BurbankTrain)
+admin.site.register(LosAngelesTrain)
+admin.site.register(OrangeCountyTrain)
+admin.site.register(AnaheimTrain)
